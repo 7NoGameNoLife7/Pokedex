da79b633edca25bd0093a6af6f809e381f8eca41
7NoGameNoLife7 <sebastien.zllegros@gmail.com>
2025-03-27 15:37:27 +0100

    props2

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,5 +1,8 @@
+interface PokemonCardProps {
+	pokemon: { name: string; imgSrc?: string };
+}
 
-function PokemonCard({ pokemon }) {
+function PokemonCard({ pokemon }: PokemonCardProps) {
 	return (
 		<>
 			<figure>
